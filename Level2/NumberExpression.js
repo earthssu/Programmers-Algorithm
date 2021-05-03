993a004f6614b66216c7ee23bf80c19bda0f54fd
earthssu <entjq9502@khu.ac.kr>
2021-05-03 09:57:22 +0900

    210503: level2

---
+++ Level2/NumberExpression.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let answer = 1;
+    let start = 1;
+    while (start < n) {
+        let sum = 0;
+        let plus = start;
+        while (sum < n) {
+            sum += plus;
+            plus += 1;
+        }
+        if (sum === n) answer += 1;
+        start += 1;
+    }
+    return answer;
+}
+
+console.log(solution(15));

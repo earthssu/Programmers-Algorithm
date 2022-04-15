ebfd24c21a00a14c3e88b3d79c883db1325c5642
earthssu <entjq9502@khu.ac.kr>
2022-04-15 17:25:53 +0900

    220415: level1/소수

---
+++ SummerWinter Coding/소수만들기.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+
+def solution(nums):
+    answer = 0
+
+    combination = list(combinations(nums, 3))
+    for com in combination:
+        c_sum = sum(com)
+        for i in range(2, c_sum):
+            if c_sum % i == 0:
+                break
+        else:
+            answer += 1
+
+    return answer
+
+
+print(solution([1,2,7,6,4]))

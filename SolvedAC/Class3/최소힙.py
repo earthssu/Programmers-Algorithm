44f3e5fe05d7638a55a1cc2b41b886ac2ffc4985
earthssu <entjq9502@khu.ac.kr>
2022-07-04 19:44:03 +0900

    220704: 백준/class3/heap

---
+++ SolvedAC/Class3/최소힙.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+
+
+heap = []
+heapq.heapify(heap)
+N = int(sys.stdin.readline())
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    if num > 0:
+        heapq.heappush(heap, num)
+    elif num == 0:
+        if heap:
+            p = heapq.heappop(heap)
+            print(p)
+        else:
+            print(0)

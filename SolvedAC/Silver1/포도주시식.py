4fb91f0f82dc4e5752a0261f4539e3f477656088
earthssu <entjq9502@khu.ac.kr>
2022-08-22 14:35:50 +0900

    220822: 백준/silver1/dynamic programming

--- SolvedAC/Silver1/포도주시식.py
+++ SolvedAC/Silver1/포도주시식.py
@@ -1,15 +1,15 @@
 import sys
 
 N = int(sys.stdin.readline())
-grape = []
-for _ in range(N):
-    grape.append(int(sys.stdin.readline()))
+grape = [0] * 10000
+for i in range(N):
+    grape[i] = int(sys.stdin.readline())
 
 dp = [0] * 10000
 dp[0] = grape[0]
 dp[1] = grape[0] + grape[1]
 dp[2] = max(grape[0]+grape[2], grape[1]+grape[2], dp[1])
 for i in range(3, N):
-    dp[i] = max(grape[i]+grape[i-2], grape[i]+grape[i-1]+grape[i-3], dp[i-1])
+    dp[i] = max(grape[i]+dp[i-2], grape[i]+grape[i-1]+dp[i-3], dp[i-1])
 
 print(max(dp))

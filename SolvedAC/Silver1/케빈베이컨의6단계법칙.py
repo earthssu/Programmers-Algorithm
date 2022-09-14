7bd15e9600c41e9c5b3a4f4230fe53316c0d2822
earthssu <entjq9502@khu.ac.kr>
2022-09-14 14:18:00 +0900

    220914: 백준/silver1/BFS

--- SolvedAC/Silver1/케빈베이컨의6단계법칙.py
+++ SolvedAC/Silver1/케빈베이컨의6단계법칙.py
@@ -1,5 +1,5 @@
 import sys
-INF = int(1e9)
+from collections import deque
 
 N, M = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
@@ -9,44 +9,25 @@ for _ in range(M):
     graph[a].append(b)
     graph[b].append(a)
 
-visited = [False] * (N+1)
-distance = [INF] * (N+1)
 
+def bfs(start):
+    distance = [0] * (N+1)
+    visited = [False] * (N+1)
+    queue = deque([start])
 
-def get_smallest_node():
-    min_value = INF
-    index = 0
-    for i in range(1, N+1):
-        if not visited[i] and distance[i] < min_value:
-            min_value = distance[i]
-            index = i
-    return index
+    while queue:
+        node = queue.popleft()
+        visited[node] = True
+        for i in graph[node]:
+            if not visited[i]:
+                distance[i] = distance[node] + 1
+                queue.append(i)
 
+    return sum(distance)
 
-def dijkstra(start):
-    distance[start] = 0
-    visited[start] = True
-
-    for i in graph[start]:
-        distance[i] = 1
-
-    for _ in range(N-1):
-        now = get_smallest_node()
-        visited[now] = True
-        for next in graph[now]:
-            cost = distance[now] + 1
-            if cost < distance[next]:
-                distance[next] = cost
 
 result = []
-for n in range(1, N+1):
-    dijkstra(n)
-    result.append(sum(distance[1:]))
-    visited = [False] * (N + 1)
-    distance = [INF] * (N + 1)
+for i in range(1, N+1):
+    result.append(bfs(i))
 
-min_num = min(result)
-for i in range(N):
-    if min_num == result[i]:
-        print(i+1)
-        break
+print(result.index(min(result))+1)

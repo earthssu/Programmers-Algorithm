35c34009c9cc857599bf8e234aa9a154d2f907ad
JisuKim <entjq9502@khu.ac.kr>
2022-10-18 16:53:24 +0900

    221018: 백준/silver4/greedy

---
+++ SolvedAC/Silver3-5/설탕배달.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+answer = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        answer += N // 5
+        print(answer)
+        break
+    N -= 3
+    answer += 1
+else:
+    print(-1)
